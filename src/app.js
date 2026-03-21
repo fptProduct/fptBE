53c92ed171408ac6aae117b523fce490f0bc863c
ThaiNguyen <Thaind@wiicamp.com>
2026-03-21 23:51:25 +0700

    test

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { v2: cloudinary } = require("cloudinary");
 require("dotenv").config();
 
 const productRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
 const seedDefaultCategories = require("./utils/categorySeeder");
 
 const app = express();
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+app.locals.cloudinary = cloudinary;
+
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(async () => {
     console.log("MongoDB connected");
     await seedDefaultCategories();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
   })
   .catch((err) => console.log(err));
 
@@ -27,8 +43,6 @@ app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/uploads", uploadRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+
